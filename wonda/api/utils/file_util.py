78ec561e41e897c444909263e74898c86724cf66
Andrei Korolev <andrewsalt.e@gmail.com>
2023-01-09 03:20:25 +0500

    refactor(api/file_util): fix typing issues & kind of add support for ids and urls

--- wonda/api/utils/file_util.py
+++ wonda/api/utils/file_util.py
@@ -2,26 +2,21 @@ from io import BytesIO
 from pathlib import Path
 from typing import Optional, Union
 
-Bytes = Union[bytes, BytesIO]
-FilePath = Union[str, Path]
-
 
 class File:
-    def __init__(
-        self, name: Optional[str] = None, content: Optional["Bytes"] = None
-    ) -> None:
-        self.name = name or "Unnamed"
-        self.content = content.getvalue() if isinstance(content, BytesIO) else content
+    def __init__(self, content: Union[str, bytes], name: Optional[str] = None) -> None:
+        self.name, self.content = name, content
 
     @classmethod
-    def from_bytes(cls, file_source: Bytes, name: Optional[str] = None) -> "File":
-        return cls(name=name, content=file_source)
-
-    @classmethod
-    def from_path(
-        cls, file_source: FilePath, name: Optional[str] = None
+    def from_bytes(
+        cls, data: Union[bytes, BytesIO], name: Optional[str] = None
     ) -> "File":
-        path = Path(file_source) if isinstance(file_source, str) else path
+        content = data.getvalue() if isinstance(data, BytesIO) else data
+        return cls(name=name or "unnamed.bin", content=content)
+
+    @classmethod
+    def from_path(cls, source: Union[str, Path], name: Optional[str] = None) -> "File":
+        path = Path(source) if isinstance(source, str) else path
 
         with open(path, "rb") as f:
             return cls(name=name or path.name, content=f.read())
