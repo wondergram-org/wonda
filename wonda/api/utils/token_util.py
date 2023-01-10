2443d600a51dbab08d03e3abe49964b5ec3ad088
Andrei Korolev <andrewsalt.e@gmail.com>
2023-01-10 19:04:33 +0500

    refactor(api/token_util): raise exception if the variable doesn't exist

--- wonda/api/utils/token_util.py
+++ wonda/api/utils/token_util.py
@@ -1,8 +1,13 @@
 from os import getenv
-from typing import Optional
+from typing import NoReturn, Optional, Union
 
 
 class Token(str):
     @classmethod
-    def from_env(cls, variable_name: Optional[str] = "WONDA_TOKEN") -> "Token":
-        return cls(getenv(variable_name))
+    def from_env(
+        cls, var_name: Optional[str] = "WONDA_TOKEN"
+    ) -> Union["Token", NoReturn]:
+        if token := getenv(var_name):
+            return cls(token)
+
+        raise KeyError(f"Environment variable {var_name!r} not found")
