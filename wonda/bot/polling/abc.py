68a03fad0b811fd0b679b9aab58cf55240192d6b
Andrei Korolev <andrewsalt.e@gmail.com>
2023-02-04 00:17:29 +0500

    style(polling/abc): remove unused imports

--- wonda/bot/polling/abc.py
+++ wonda/bot/polling/abc.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Any, AsyncIterator, Generator, Optional
+from typing import AsyncIterator
 
 from wonda.api.abc import ABCAPI
 from wonda.errors import ABCErrorHandler
