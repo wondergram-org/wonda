9a42090b89152d56fd6b6a2beb3c7ae74ab62808
Andrei Korolev <andrewsalt.e@gmail.com>
2023-02-04 00:12:27 +0500

    fix(file_upload_example): make  `send_chat_action` call a little bit earlier

--- examples/high_level/file_upload_example.py
+++ examples/high_level/file_upload_example.py
@@ -17,11 +17,11 @@ async def upload_handler(msg: Message) -> None:
         chat_id=msg.chat.id, caption="Lookin' tasty!", photo=SAMPLE_PHOTO
     )
 
-    # Download a sample image from Lorem Picsum using the HTTP client.
-    content = await msg.ctx_api.http_client.request_content("https://picsum.photos/300")
-
     # While we download the image, let the user know something's brewing.
     await msg.ctx_api.send_chat_action(chat_id=msg.chat.id, action="upload_photo")
+
+    # Download a sample image from Lorem Picsum using the HTTP client.
+    content = await msg.ctx_api.http_client.request_content("https://picsum.photos/300")
 
     # Construct a file using <.from_bytes(...) method. It has "unnamed.bin"
     # name by default, so it's advised to give it a proper name.
@@ -34,7 +34,7 @@ async def upload_handler(msg: Message) -> None:
 
     # To enable faster uploads, use File directly. Supply it with a link to a file
     # or a file_id and Telegram will take care of the rest.
-    larger_photo = File("https://picsum.photos/1000.webp")
+    larger_photo = File("https://picsum.photos/1000")
 
     # Despite the image being a lot larger,
     # it will be uploaded just as fast as the previous one!
