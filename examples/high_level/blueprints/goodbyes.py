0f10feea4ebd0f5862299bb9b2d37190d76c889a
Andrei Korolev <andrewsalt.e@gmail.com>
2023-08-08 04:09:24 +0500

    fix(examples/blueprints): replace returns with shortcut method calls

--- examples/high_level/blueprints/goodbyes.py
+++ examples/high_level/blueprints/goodbyes.py
@@ -1,4 +1,4 @@
-from wonda import Blueprint
+from wonda import Blueprint, Message
 from wonda.bot.rules import Text
 
 # Make a blueprint of a bot.
@@ -6,10 +6,10 @@ bp = Blueprint()
 
 
 @bp.on.message(Text(["bye", "cheers"], ignore_case=True))
-async def bye_handler(_) -> str:
-    return "See you soon!"
+async def bye_handler(m: Message) -> None:
+    await m.answer("See you soon!")
 
 
 @bp.on.message(Text(["take care", "have a good day"], ignore_case=True))
-async def goodbye_handler(_) -> str:
-    return "You too, bye!"
+async def goodbye_handler(m: Message) -> None:
+    await m.answer("You too, bye!")
--- examples/high_level/blueprints/greetings.py
+++ examples/high_level/blueprints/greetings.py
@@ -1,4 +1,4 @@
-from wonda import Blueprint
+from wonda import Blueprint, Message
 from wonda.bot.rules import Text
 
 # Make a blueprint of a bot.
@@ -6,12 +6,12 @@ bp = Blueprint()
 
 
 @bp.on.message(Text(["hi", "hello", "hey"], ignore_case=True))
-async def hi_handler(_) -> str:
-    return "Glad to see you!"
+async def hi_handler(m: Message) -> None:
+    await m.answer("Glad to see you!")
 
 
 @bp.on.message(
     Text(["good morning", "good afternoon", "good evening"], ignore_case=True)
 )
-async def hello_handler(_) -> str:
-    return "Welcome!"
+async def hello_handler(m: Message) -> None:
+    await m.answer("Welcome!")
